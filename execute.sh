41d0d9f1156417a09fc68436b94999fc24217120
ILakshitha <140165538+ILakshitha@users.noreply.github.com>
2024-09-21 15:53:55 +0530

    edit execute file

--- execute.sh
+++ execute.sh
@@ -1,55 +1,20 @@
 #!/bin/bash
 
-# Ensure the out directory exists
+# Ahoy! Set up the seas of your Git repo!
+# Ensure the out directory exists for the commits.txt treasure
 mkdir -p out
 
-# File to store the commit logs
-LOGFILE="out/commits.txt"
+# Get the latest commit info and log it to the commits.txt treasure map
+commit_hash=$(git log -1 --pretty=format:"%H")
+commit_message=$(git log -1 --pretty=format:"%s")
+commit_author=$(git log -1 --pretty=format:"%an")
+commit_date=$(git log -1 --pretty=format:"%ad")
 
-# Function to add a pirate flair to commit messages
-add_pirate_flair() {
-    local message="$1"
-    local pirate_phrases=("Arrr!" "Shiver me timbers!" "Yo ho ho!" "Avast ye!" "Blimey!")
-    local random_phrase=${pirate_phrases[$RANDOM % ${#pirate_phrases[@]}]}
-    echo "$random_phrase $message"
-}
+echo "Arrr, a new commit be here! ⚓" >> out/commits.txt
+echo "Commit: $commit_hash" >> out/commits.txt
+echo "Message: $commit_message" >> out/commits.txt
+echo "Author: $commit_author" >> out/commits.txt
+echo "Date: $commit_date" >> out/commits.txt
+echo "-----------------------------------" >> out/commits.txt
 
-# Get the initial HEAD commit hash
-last_commit=$(git rev-parse HEAD)
-
-echo "Ahoy! Captain Hook's commit logger be ready to chart yer code voyage!"
-
-# Set a time limit (in seconds) - 4 minutes 30 seconds to allow for some buffer
-end_time=$((SECONDS + 270))
-
-while [ $SECONDS -lt $end_time ]; do
-    # Check for new commits
-    current_commit=$(git rev-parse HEAD)
-    
-    if [ "$current_commit" != "$last_commit" ]; then
-        # New commit found! Log it
-        commit_message=$(git log -1 --pretty=%B)
-        commit_author=$(git log -1 --pretty=%an)
-        commit_date=$(git log -1 --pretty=%ad --date=format:'%Y-%m-%d %H:%M:%S')
-
-        # Add pirate flair to the commit message
-        pirate_message=$(add_pirate_flair "$commit_message")
-
-        # Log the commit details
-        echo "⚓ Commit: $current_commit" >> "$LOGFILE"
-        echo "🏴‍☠️ Message: $pirate_message" >> "$LOGFILE"
-        echo "👤 Author: $commit_author" >> "$LOGFILE"
-        echo "🕰️ Date: $commit_date" >> "$LOGFILE"
-        echo "---" >> "$LOGFILE"
-
-        echo "Yarr! New treasure be added to the map!"
-        
-        # Update last commit
-        last_commit=$current_commit
-    fi
-
-    # Wait a bit before checking again (reduced from 5 to 1 second)
-    sleep 1
-done
-
-echo "Avast ye! The time limit be reached. Ending the script."
+echo "Ahoy, matey! Commit $commit_hash be added to the log!" 
